206e77a6bf300ee19495ffd813b4377c8f6562c5
Patrícia <prrs89@gmail.com>
2019-09-29 17:51:02 +0100

    feat: adds icons packages

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faAt } from '@fortawesome/free-solid-svg-icons';
+import './css/Footer.css';
 
-function Footer(props) {
+function Footer() {
     return (
-        <footer>
+        <footer className="footer">
             <h2>Let's connect!</h2>
 
-            <a href="https://www.linkedin.com/in/patriciarrsilva/?locale=en_US" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <a href="https://github.com/patriciarrsilva" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <a href="mailto:prrs89@gmail.com">eMail</a>
+            <div className="contacts">
+                <a href="https://www.linkedin.com/in/patriciarrsilva/?locale=en_US" target="_blank" rel="noopener noreferrer" className="contacts__icon linkedin-icon">
+                    <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
+                <a href="https://github.com/patriciarrsilva" target="_blank" rel="noopener noreferrer" className="contacts__icon github-icon">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="mailto:prrs89@gmail.com" className="contacts__icon email-icon">
+                    <FontAwesomeIcon icon={faAt} />
+                </a>
+            </div>
 
-            <address>
+            <address className="address">
                 <p>Patrícia Silva</p>
-                <p>Porto, Portugal | 2019</p>
+                <p>Porto, Portugal (2019)</p>
             </address>
         </footer>
     )
